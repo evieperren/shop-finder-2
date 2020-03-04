e969d237fcd8f8791ece32d649c425ad9059fa7e
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-03-04 22:35:27 +0000

    trying joi for validation

--- src/controllers/shop-controller.js
+++ src/controllers/shop-controller.js
@@ -19,8 +19,10 @@ ShopController.post('/', (req, res, next) => {
     type: req.body.type,
     location: {
       postcode: req.body.location.postcode,
-      town: req.body.location.town
-    }
+      town: req.body.location.town,
+      online: req.body.location.online
+    },
+    scale: req.body.scale
   })
   try {
     res.send(shop)
@@ -33,8 +35,12 @@ ShopController.post('/', (req, res, next) => {
 
 // get all 
 ShopController.get('/', async (req, res, next) => {
-  const result = await Shop.find()
     try {
+      const result = await Shop.find()
+
+      if(!result || result === undefined){
+        throw new Error('There are no shops in database')
+      }
       res.json(result)
       
     } catch (error){
@@ -44,36 +50,47 @@ ShopController.get('/', async (req, res, next) => {
 
 // get one
 ShopController.get('/:id', async (req, res, next) => {
-  const result = await Shop.findById(req.params.id)
-    .then (() => res.json(result))
-    .catch((error) => console.log( new Error(`Error: ${error} `)))
+  try {
+    const result = await Shop.findById(req.params.id)
+
+    res.json(result)
+  } catch (error) {
+    console.log( new Error(`Could not find shop. ${error} `))
+  }
 })
 
 // update
 ShopController.put('/:id', async (req, res, next) => {
-  await Shop.findById(req.params.id)
-    .then((returnedShop) => {
-      returnedShop.name = req.body.name || returnedShop.name
-      returnedShop.type = req.body.type || returnedShop.type
-      // must include one of these within a change ??
-      returnedShop.location.postcode = req.body.location.postcode || returnedShop.location.postcode
-      returnedShop.location.town = req.body.location.town || returnedShop.location.town
+  try {
+    const returnedShop = await Shop.findById(req.params.id)
 
-      returnedShop.save()
-      res.send(returnedShop)
-    })
-    .catch((error) => console.log( new Error(`Error: ${error} `)))
+    returnedShop.name = req.body.name || returnedShop.name
+    returnedShop.type = req.body.type || returnedShop.type
+    // must include one of these within a change ??
+    returnedShop.location.postcode = req.body.location.postcode || returnedShop.location.postcode
+    returnedShop.location.town = req.body.location.town || returnedShop.location.town
+    returnedShop.location.town = req.body.location.online || returnedShop.location.online
+    returnedShop.scale = req.body.scale || returnedShop.scale
+  
+    returnedShop.save()
+    res.send(returnedShop)
+
+  } catch (error) {
+    console.log( new Error(`Error: ${error} `))
+  }
 })
 
 
 // delete 
 ShopController.delete('/:id', async (req, res, next) => {
-  await Shop.findByIdAndRemove(req.params.id)
-    .then (() => {
-      res.json({
-        "message": `${req.params.id} has been successfully`
-      })
+  try {
+    await Shop.findByIdAndRemove(req.params.id)
+    res.json({
+      "message": `${req.params.id} has been successfully`
     })
-    .catch((error) => console.log( new Error(`Error: ${error} `)))
+
+  } catch (error){
+    console.log( new Error(`Error: ${error} `))
+  }
 })
 module.exports = ShopController
--- src/schema/shop.js
+++ src/schema/shop.js
@@ -1,12 +1,59 @@
 const mongoose = require('mongoose')
+const Joi = require('@hapi/joi')
 
-const ShopSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  type: String,
+const ShopSchema  = Joi.object().keys({
+  name: Joi.string()
+    .required(),
+  type: Joi.string()
+    .required(),
   location: {
-    postcode: String,
-    town: String
-  }
-})
+    postcode: Joi.string()
+      .required()
+      .uppercase()
+      .trim(),
+    town: Joi.string()
+      .required()
+      .uppercase()
+      .trim(),
+    online: Joi.string()
+      .required()
+      .uppercase()
+      .trim()
+  },
+  scale: Joi.string()
+    .required()
+    .valid('small', 'medium', 'large')
 
+})
+// const ShopSchema = mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   type: {
+//     type: String,
+//     required: true
+//   },
+//   location: {
+//     postcode: {
+//       type: String,
+//       uppercase: true,
+//       trim: true
+//     },
+//     town: {
+//       type: String,
+//       uppercase: true,
+//       trim: true
+//     },
+//     online: {
+//       type: Boolean,
+//       required: true
+//     }
+//   },
+//   scale: {
+//     type: String,
+//     required: false,
+//     enum: ['small', 'medium', 'large']
+//   }
+// })
 module.exports = ShopSchema
